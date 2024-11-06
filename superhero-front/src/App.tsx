99204a6e772fe2d2df5337eb63636636a5be912f
Stepan Stadniuk <stepanigcom@gmail.com>
2024-11-06 10:49:45 +0200

    + footer

--- superhero-front/src/App.tsx
+++ superhero-front/src/App.tsx
@@ -1,13 +1,18 @@
 import { Outlet } from "react-router-dom"
 import Header from "./components/Header"
+import Footer from "./components/Footer"
+import { Box } from "@mui/material"
 
 function App() {
 
   return (
-    <>
+    <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
      <Header />
-     <Outlet />
-    </>
+     <Box sx={{ flex: 'auto' }}>
+      <Outlet />
+     </Box>
+     <Footer />
+    </Box>
   )
 }
 
---
+++ superhero-front/src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+import { Divider, Typography } from "@mui/material";
+
+export default function Footer() {
+  return (
+    <div>
+      <Divider />
+      <div>
+        <Typography textAlign="center" sx={{ py: 1 }}>Superhero 2024</Typography>
+      </div>
+    </div>
+  )
+}
